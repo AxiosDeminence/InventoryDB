5267ecb7c3363506519b2756753be0ed08e83132
Juhmer Tena <juhmer@att.net>
2020-11-17 18:20:15 -0800

    Add endpoints for character management and item management. Removed deprecated endpoint.

--- inventory_management/backend/urls.py
+++ inventory_management/backend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserVisit, CharacterInventory
+from .views import UserVisit, ItemManagement, CharacterManagement
 
 app_name = "backend"
 
 urlpatterns = [
     path("users/", UserVisit.as_view()),
-    path("inventory/", CharacterInventory.as_view()),
+    path("items/", ItemManagement.as_view()),
+    path("characters/", CharacterManagement.as_view()),
 ]
--- inventory_management/backend/views.py
+++ inventory_management/backend/views.py
@@ -4,13 +4,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.utils.datastructures import MultiValueDictKeyError as MissingParamError
+
 from .models import User, Character, Item
 from .serializers import (
     UserCreationSerializer,
     CharacterCreationSerializer,
     ItemCreationSerializer,
-    ItemInfoSerializer,
-    CharacterInventorySerializer,
     UserDataSerializer,
 )
 
@@ -25,11 +25,133 @@ class UserVisit(APIView):
             })
         
         serializer = UserDataSerializer(client, read_only=True)
-        return Response(serializer.data["character_inventories"])
+        return Response(status=status.HTTP_200_OK, data=serializer.data["character_inventories"])
 
-class CharacterInventory(APIView):
+class ItemManagement(APIView):
+    # Item Creation
     def post(self, request, format=None):
-        queryset = Character.objects.get(name=request.data.get("character")).item_set.all()
-        serializer = ItemInfoSerializer(queryset, many=True, read_only=True)
+        try:
+            data = {
+                "name": request.data["name"],
+                "type": request.data["type"],
+                "enhancements": request.data["enhancements"],
+                "quantity": request.data["quantity"],
+                "owner": request.data["owner"],
+            }
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Missing parameters.",
+            })
+        
+        serializer = ItemCreationSerializer(data=data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Incorrect form usage."
+            })
+        
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED, data={
+            "message": "Object created.",
+        })
 
-        return Response(serializer.data)
+    # Item Edits
+    def patch(self, request, format=None):
+        try:
+            data = {
+                "id": request.data["id"],
+                "name": request.data["name"],
+                "type": request.data["type"],
+                "enhancements": request.data["enhancements"],
+                "quantity": request.data["quantity"],
+                "owner": request.data["owner"],
+            }
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Missing parameters.",
+            })
+        
+        serializer = ItemCreationSerializer(data=data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Incorrect form usage."
+            })
+        
+        item = Item.objects.get(id=data["id"])
+        serializer.update(item, serializer.validated_data)
+        return Response(status=status.HTTP_201_CREATED, data={
+            "message": "Object updated.",
+        })
+
+
+    # Item Deletion
+    def delete(self, request, format=None):
+        try:
+            data = {
+                "id": request.data["id"],
+            }
+            item = Item.objects.select_related("owner__owner").get(id=data["id"])
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Missing parameters.",
+            })
+        except Item.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Item must be valid.",
+            })
+
+        if item.owner.owner != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data={
+                "message": "Cannot delete item that is not yours."
+            })
+
+        item.delete()
+        return Response(status=status.HTTP_202_ACCEPTED, data={
+            "message": "Item deleted.",
+        })
+
+class CharacterManagement(APIView):
+    def post(self, request, form=None):
+        try:
+            data = {
+                "name": request.data["name"],
+            }
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Missing parameters.",
+            })
+        
+        serializer = CharacterCreationSerializer(data=data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Incorrect form usage or character already exists."
+            })
+        
+        serializer.save(owner=request.user)
+        return Response(status=status.HTTP_201_CREATED, data={
+            "message": "Character created."
+        })
+    
+    def delete(self, request, form=None):
+        try:
+            data = {
+                "name": request.data["name"],
+            }
+            char = Character.objects.select_related("owner").get(name=data["name"])
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Missing parameters.",
+            })
+        except Character.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={
+                "message": "Character must be valid.",
+            })
+        
+        if char.owner != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data={
+                "message": "Cannot delete item that is not yours."
+            })
+        
+        char.delete()
+        return Response(status=status.HTTP_202_ACCEPTED, data={
+            "message": "Character deleted.",
+        })
